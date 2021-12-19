2fd49c0b9940048762a520125e271ccd6b60b603
bmteasdale <bmteasdale3@gmail.com>
2021-12-19 12:39:48 -0400

    add new logo design to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export default function NavBar() {
     return (
         <div>
             <Navbar bg="light" variant="light" id="navbar">
-                <Navbar.Brand href="/"><img className="navbar-logo" alt="AHS_Logo" src={require("../res/AHS_LOGO_W&G-compressed.jpg")} /></Navbar.Brand>
+                <Navbar.Brand href="/"><img className="navbar-logo" alt="AHS_Logo" src={require("../res/AHS Logo.png")} /></Navbar.Brand>
                 <Nav className="navbar-nav ml-auto">
                     <Nav.Link className="nav-item" href="#contact">
                         <Button variant="outline-danger">Contact Us</Button>
